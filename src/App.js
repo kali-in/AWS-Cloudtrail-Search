f882d017d09df7462a88b30c8afaab0e3d979bd1
kali-in <kali_in@protonmail.com>
2024-09-16 07:01:17 +0000

    Fixed typo due to merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import React, { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
